967fb757c30c4eb427990d9cdf23504aad668c2b
François Rouault <francois.rouault@cocoricostudio.com>
2022-03-07 21:30:48 +0100

    chore: move types in index file (single file)

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,8 @@ import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import { unified } from 'unified';
 import { visit } from 'unist-util-visit';
-
-import { NextMdConfig, YAMLFrontMatter } from './types';
-
 import type { Root } from 'mdast';
+
 const NextMd = <PageFrontMatter extends YAMLFrontMatter, PostPageFrontMatter extends PageFrontMatter>(
   config: NextMdConfig,
 ) => {
@@ -330,4 +328,26 @@ function extractDataFromAlt(alt: string) {
 }
 
 export default NextMd;
-export { rehypeVideos as rehypeVideos };
+
+// -----------
+// Types
+// -----------
+
+export type NextMdConfig = {
+  /**
+   * The place where to find your markdown files and folders.
+   *
+   * This is mandatory.
+   */
+  pathToContent: string;
+
+  /**
+   * The place where your markdown files are stored.
+   *
+   * - if empty / undefined, your markdown files are considered to be in your current project at path `pathToContent`.
+   * - if specified, nextmd will pull or clone the repository and look for markdown files at path `pathToContent` in that repo.
+   */
+  contentGitRemoteUrl?: string;
+};
+
+export type YAMLFrontMatter = { [key: string]: any };
