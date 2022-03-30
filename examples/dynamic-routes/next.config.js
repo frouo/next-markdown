a2f522af665051cbb4bb10ab095811d929a06708
Edvin CANDON <edvincandon@gmail.com>
2022-03-30 12:21:35 +0200

    chore: fix examples next/eslint/babel config files

--- examples/dynamic-routes/next.config.js
+++ examples/dynamic-routes/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    externalDir: true,
+    esmExternals: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
