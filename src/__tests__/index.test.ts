015fa5338656d12bf06afcc18222dea030918a37
François Rouault <francois.rouault@cocoricostudio.com>
2022-04-05 19:14:47 +0200

    refactor: move markdown.ts tests in a markdown.test.ts

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,5 +1,4 @@
 import NextMarkdown, { Config } from '..';
-import { markdownToHtml, parseMarkdownFileContent } from '../utils/markdown';
 
 describe('next-markdown', () => {
   test('loads correctly', () => {
@@ -12,30 +11,5 @@ describe('next-markdown', () => {
   test('index exports Config type', () => {
     const config: Config<{}> = { pathToContent: 'test/path' };
     expect(config).toBeDefined();
-  });
-});
-
-describe('markdown.ts', () => {
-  test('extracts front matter from md file content', () => {
-    const mdFileContent = "---\ntitle: 'I am a title'\n---\n# Heading";
-    const { frontMatter, content } = parseMarkdownFileContent<{ title: string }>(mdFileContent);
-
-    expect(frontMatter?.title).toBe('I am a title');
-    expect(content).toBe('# Heading');
-  });
-
-  test('frontMatter is empty when md file content does not have front matter', () => {
-    const mdFileContent = '# Heading';
-    const { frontMatter, content } = parseMarkdownFileContent(mdFileContent);
-
-    expect(frontMatter).toEqual({});
-    expect(content).toBe('# Heading');
-  });
-
-  test('converts markdown to HTML', async () => {
-    const mdFileContent = '# Present yourself\n\nI am **next-markdown**';
-    const html = await markdownToHtml(mdFileContent);
-
-    expect(html).toBe('<h1 id="present-yourself">Present yourself</h1>\n<p>I am <strong>next-markdown</strong></p>');
   });
 });
---
+++ src/__tests__/markdown.test.ts
@@ -0,0 +1,28 @@
+import { markdownToHtml, parseMarkdownFileContent } from '../utils/markdown';
+
+describe('parse markdown file', () => {
+  test('extracts front matter from md file content', () => {
+    const mdFileContent = "---\ntitle: 'I am a title'\n---\n# Heading";
+    const { frontMatter, content } = parseMarkdownFileContent<{ title: string }>(mdFileContent);
+
+    expect(frontMatter?.title).toBe('I am a title');
+    expect(content).toBe('# Heading');
+  });
+
+  test('frontMatter is empty when md file content does not have front matter', () => {
+    const mdFileContent = '# Heading';
+    const { frontMatter, content } = parseMarkdownFileContent(mdFileContent);
+
+    expect(frontMatter).toEqual({});
+    expect(content).toBe('# Heading');
+  });
+});
+
+describe('markdown to html', () => {
+  test('converts markdown to HTML', async () => {
+    const mdFileContent = '# Present yourself\n\nI am **next-markdown**';
+    const html = await markdownToHtml(mdFileContent);
+
+    expect(html).toBe('<h1 id="present-yourself">Present yourself</h1>\n<p>I am <strong>next-markdown</strong></p>');
+  });
+});
